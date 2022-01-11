b21b5fca4c6748dc005368c043a35464bf6d9220
davioliveira8700 <odavi278@gmail.com>
2022-01-10 21:05:29 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,14 @@
+﻿using ExemploPoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+   public class Aluno : Pessoa
+    {
+        public int Nota { get; set; }
+    }
+}
---
+++ Professor.cs
@@ -0,0 +1,15 @@
+﻿using ExemploPoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExemploPOO.Models
+{
+    class Professor : Pessoa
+    {
+        public double Salario { get; set; }
+    }
+}
